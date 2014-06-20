dc18c2f6c066ab736d6193e94b71dbdced55d2fa
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-06-20 14:43:45 +0100

    fixed reddit searcher

--- src/main/java/com/learnedsomething/dao/impl/RedditDaoImpl.java
+++ src/main/java/com/learnedsomething/dao/impl/RedditDaoImpl.java
@@ -12,11 +12,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.NumberUtils;
 import org.springframework.util.StringUtils;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -78,15 +76,11 @@ public class RedditDaoImpl implements SearchDao {
         String rank = rawRank.getText();
 
         if (StringUtils.hasText(rank)) {
-            Integer down = NumberUtils.parseNumber(rawLink.getAttribute("data-downs"), Integer.class);
-            Integer up = NumberUtils.parseNumber(rawLink.getAttribute("data-ups"), Integer.class);
             WebElement rawEntry = rawLink.findElement(By.className("entry"));
             WebElement rawTitle = rawEntry.findElement(By.cssSelector("a.title"));
-            WebElement rawComments = rawEntry.findElement(By.cssSelector("a.comments"));
 
             String uri = rawTitle.getAttribute("href");
             String text = rawTitle.getText();
-            String commentsUri = rawComments.getAttribute("href");
 
             if (StringUtils.hasText(uri) && StringUtils.hasText(text)) {
                 link.setUri(uri);
@@ -94,19 +88,6 @@ public class RedditDaoImpl implements SearchDao {
             }
         }
         return link;
-    }
-
-    private Date dateFromString(WebElement rawEntry) {
-        Date date = null;
-        try {
-            WebElement rawTime = rawEntry.findElement(By.cssSelector("time"));
-            String dateStr = rawTime.getAttribute("datetime");
-            date = dateFormatter.parse(dateStr);
-        } catch (Exception e) {
-            LOG.error("Can't convert date", e);
-            date = new Date();
-        }
-        return date;
     }
 
     private void processPaginationUris(SearchResult searchResult, List<WebElement> uris) {
